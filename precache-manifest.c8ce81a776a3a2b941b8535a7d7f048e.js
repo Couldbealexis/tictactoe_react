db07c34b8257a3cf3660350f64d423a0abfc0578
Alexis Gutierrez <couldbealexis@icloud.com>
2020-05-24 00:03:16 -0500

    Updates

---
+++ precache-manifest.c8ce81a776a3a2b941b8535a7d7f048e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc019ab08007dd1741526884497476ed",
+    "url": "/tictactoe_react/index.html"
+  },
+  {
+    "revision": "c85efd7ebf9fae749733",
+    "url": "/tictactoe_react/static/css/main.7bede077.chunk.css"
+  },
+  {
+    "revision": "8bb9b1a2e35ba0462042",
+    "url": "/tictactoe_react/static/js/2.b4039258.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe_react/static/js/2.b4039258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c85efd7ebf9fae749733",
+    "url": "/tictactoe_react/static/js/main.491bcf59.chunk.js"
+  },
+  {
+    "revision": "190e37714b43208a3abd",
+    "url": "/tictactoe_react/static/js/runtime-main.f105bf8b.js"
+  }
+]);
