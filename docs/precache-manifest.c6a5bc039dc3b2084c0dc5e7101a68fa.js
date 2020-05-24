85db8c39d45a2ff441f125bb5ef1a1a231e328b2
Alexis Gutierrez <couldbealexis@icloud.com>
2020-05-23 23:09:09 -0500

    github pages

---
+++ docs/precache-manifest.c6a5bc039dc3b2084c0dc5e7101a68fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b4544f13dd2a02fd9313886e36e040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa69984d816c7058fdc2",
+    "url": "/static/css/main.7bede077.chunk.css"
+  },
+  {
+    "revision": "ef163c8a1d8790fa9b7c",
+    "url": "/static/js/2.65ce77cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65ce77cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa69984d816c7058fdc2",
+    "url": "/static/js/main.96af171e.chunk.js"
+  },
+  {
+    "revision": "1dbc08d5af3c7ec1d49a",
+    "url": "/static/js/runtime-main.81823a7a.js"
+  }
+]);
